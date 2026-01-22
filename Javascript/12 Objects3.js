7946225a3c1a20d7f415944ff33c07ccaf26106c
bipesh369 <juniorbipesh@gmail.com>
2026-01-22 07:11:05 +0000

    Learned about object and api

---
+++ Javascript/12 Objects3.js
@@ -0,0 +1,23 @@
+// Object Destructuring and JSON API
+
+const course = {
+  coursename: "Javascript",
+  price: "999",
+  courseinstructor: "Bipesh",
+}
+
+// course.courseinstructor
+
+// Object Destructring
+const {courseinstructor: instructor} = course
+//console.log(courseinstructor)
+console.log(instructor)
+
+
+// API comes from backend like given below
+[
+  {},   
+  {},
+  {},
+  {}
+]
