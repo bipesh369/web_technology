d3440a9714334ec63a28e7a23e4c202f6f1ef8c9
bipesh369 <juniorbipesh@gmail.com>
2026-01-24 07:27:01 +0000

    Learned about while, do-while loop

---
+++ Javascript/22 Loops.js
@@ -0,0 +1,25 @@
+// While Loops, Do while loop
+
+// let index = 0
+// while (index <= 10) {
+//   console.log(`value of index is: ${index}`)
+//   index = index + 2
+// }
+
+// Array
+
+// const friends = ["Bipesh", "Sudip", "Rajib"]
+// let i = 0
+// while (i < friends.length) {
+//   console.log(`name of firends are: ${friends[i]}`)
+//   i++
+// }
+
+
+// Do-While loop
+
+let score = 0
+do {
+  console.log(`value of score is: ${score}`)
+  score++
+} while (score <= 10)
