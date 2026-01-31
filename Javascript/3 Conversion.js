8ee4ed101e2d08b3b0ffda7750a6d8ae6c1fd103
bipesh369 <juniorbipesh@gmail.com>
2026-01-31 04:20:03 +0000

    update: explict and implicit conversion

--- Javascript/3 Conversion.js
+++ Javascript/3 Conversion.js
@@ -1,8 +1,24 @@
-let score = null
-console.log(typeof score);
+// let score = null
+// console.log(typeof score);
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// Explict Conversion
+// Use Number() when you want strict behavior.
+// Use parseInt or parseFloat when you are reading user input like "10px".
+
+// parseInt("10px") // 10
+// Number("10px")   // NaN
 
 
+//Anything → Boolean
+//This is the most important conversion in real apps.
+// Boolean(1)    // true
+// Boolean(0)    // false
+// Boolean("")   // false
+// Boolean("hi") // true
+// Boolean("0") // true
+// Boolean([])  // true
+// Boolean({})  // true
