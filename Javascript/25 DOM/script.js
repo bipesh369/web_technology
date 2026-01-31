233613878509cefe2768021bdd53920b3e1ccf71
bipesh369 <juniorbipesh@gmail.com>
2026-01-31 15:01:29 +0000

    update: single selector and multiple selector

--- Javascript/25 DOM/script.js
+++ Javascript/25 DOM/script.js
@@ -1,3 +1,14 @@
 
 const title = document.getElementById('title')
 console.log(title)
+
+//querySelector returns the first match
+document.querySelector("#title");
+document.querySelector(".box");
+document.querySelector("p");
+
+// select multiple elements
+document.querySelectorAll(".item");
+
+
+
