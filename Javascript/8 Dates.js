3a563f9543bd4f155fd68a3a1f1339bac0c09645
bipesh369 <juniorbipesh@gmail.com>
2026-01-31 06:32:59 +0000

    update: note about dates

--- Javascript/8 Dates.js
+++ Javascript/8 Dates.js
@@ -1,3 +1,9 @@
+// Dates are Objects, not Primitives
+// That means:
+// They live in memory
+// They are compared by reference
+// They can surprise you if you treat them like numbers or strings
+
 let myDate = new Date()
 console.log(myDate.toString());
 console.log(myDate.toDateString());
@@ -6,6 +12,15 @@ console.log(myDate.toJSON());
 console.log(myDate.toLocaleDateString());
 console.log(myDate.toLocaleString());
 console.log(myDate.toTimeString());
+
+let myCreateDate = new Date("01-19-2026")
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreateDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
 
 
 
