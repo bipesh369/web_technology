0f73804331ea00f21a5c8a12755828dd3fe6d11c
bipesh369 <juniorbipesh@gmail.com>
2026-01-31 04:35:19 +0000

    update: strict and loose comparision and object-array comparision

--- Javascript/4 Comparision.js
+++ Javascript/4 Comparision.js
@@ -8,7 +8,40 @@ console.log(undefined < 0);  // avoid all this type of comparision
 
 
 
+//There are two families of comparison:
 
+//Loose comparison → ==
+// "" == 0        // true
+// false == 0    // true
+// null == undefined // true
+// null == undefined   // true
+// NaN == NaN  // false
 
+//Strict comparison → ===
+// === asks two questions:
+// Same value?
+// Same type?
+// "5" === 5   // false
+// 5 === 5     // true
+// null === undefined // false
+// NaN === NaN // false // Because NaN means “not equal to anything, including itself”.
 
+//Everything confusing comes from mixing these two.
+
+// Objects and arrays (common trap)
+// [] == []   // false
+// {} == {}   // false
+// Because objects are compared by reference, not value.
+
+// Real-world example (React bug classic)
+// if (items.length == 0) {
+//   // empty
+// }
+// Works… until length is "0".
+
+// Correct:
+// if (items.length === 0) {
+//   // empty
+// }
+// Now it’s bulletproof.
 
