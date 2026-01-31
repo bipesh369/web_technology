29b6518f5ecd1356a13aea15407c6c8019d001d6
bipesh369 <juniorbipesh@gmail.com>
2026-01-31 16:43:43 +0000

    update

--- Javascript/99practice/exercise2.js
+++ Javascript/99practice/exercise2.js
@@ -1,8 +1,28 @@
 // discount-calculator
-
 function calculatorDiscount (price) {
   discount = 0.20
   console.log(price * (1 - discount))
 }
-
 calculatorDiscount(5000)
+
+// tax-calculator.js
+function calculatorTax(price) {
+  discount = 0.05
+  console.log(price * (1 + discount))
+}
+calculatorTax(5000)
+
+
+// checkout
+let itemPrice = 100
+let discountedPrice = calculatorDiscount(itemPrice)
+console.log("After Discount:", discountedPrice)
+
+
+// Later in the code 
+let taxPrice = calculatorTax(itemPrice)
+console.log("With Tax:", taxPrice)
+
+// But now if we call calculateDiscount again:
+let newPrice = calculatorDiscount(200)
+console.log("bug! Expected 160, got:", newPrice)
