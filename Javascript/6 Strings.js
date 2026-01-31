4997f01137af192efacec33848f38d744c526db4
bipesh369 <juniorbipesh@gmail.com>
2026-01-31 06:15:43 +0000

    update: immutable string and how string actually change

--- Javascript/6 Strings.js
+++ Javascript/6 Strings.js
@@ -1,2 +1,25 @@
-const name = "Bipesh";
-const repoCount = 6;
+// Strings are immutable
+// Immutable means:
+// you cannot change a string
+// You can only create a new one 
+
+let name = "Bipesh"
+name[0] = "S"
+console.log(name) // Bipesh
+// Does nothing:
+// JS did not finally
+// It politely ignored you
+
+
+// How changing a string actually works
+let text = "hi,"
+text = text + "there"
+console.log(text)
+//what really happened:
+// "Hi" stayed untouched
+// "Hi there" was created
+// text now points to the new stirng
+
+
+//const name = "Bipesh";
+//const repoCount = 6;
