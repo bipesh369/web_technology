fa094afd8bc4d4499419d0e76fc2f45cf78982e9
bipesh369 <juniorbipesh@gmail.com>
2026-02-11 14:16:43 +0000

    practise about function

---
+++ Javascript/99practice/exercise5.js
@@ -0,0 +1,15 @@
+// count characters
+// return length of string
+function fullName(name) {
+  return name.length
+}
+let count = fullName("bipesh")
+console.log(count)
+
+
+// function that converts text to uppercase
+function name(text){
+  return text.toUpperCase()
+}
+let up = name("bipesh")
+console.log(up) 
