db8174597232918f1326409fb095ed64da14b679
bipesh369 <juniorbipesh@gmail.com>
2026-02-17 11:16:01 +0000

    do exercise of advanced reduce

---
+++ Javascript/99practice/exercise15.js
@@ -0,0 +1,8 @@
+const fruits = ["apple","banana","apple","mango","banana"];
+
+const count = fruits.reduce((acc, fruit) => {
+  acc[fruit] = (acc[fruit] || 0) + 1;
+  return acc;
+}, {});
+
+console.log(count);
